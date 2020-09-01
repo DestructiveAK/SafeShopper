46e6daaf35a3fd95f1d7ad29b7de3ceee997eac5
Aman <aksinha.101010@gmail.com>
2020-09-01 16:16:01 +0530

    fixed text color to black

--- lib/utils/input_field.dart
+++ lib/utils/input_field.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class InputField extends StatelessWidget {
-  const InputField({
-    Key key,
-    @required this.hintText,
-    @required this.icon,
-    this.isObscureText = false,
-    this.keyboardType = TextInputType.visiblePassword,
-    @required this.onChange
-  }) : super(key: key);
+  const InputField(
+      {Key key,
+      @required this.hintText,
+      @required this.icon,
+      this.isObscureText = false,
+      this.keyboardType = TextInputType.visiblePassword,
+      @required this.onChange})
+      : super(key: key);
 
   final String hintText;
   final IconData icon;
@@ -24,17 +24,25 @@ class InputField extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
       width: size.width * 0.8,
       decoration: BoxDecoration(
-          color: Color(0xFFF1E6FF), borderRadius: BorderRadius.circular(30)),
+        color: Color(0xFFF1E6FF),
+        borderRadius: BorderRadius.circular(30),
+      ),
       child: TextField(
         obscureText: isObscureText,
         keyboardType: keyboardType,
         cursorColor: Color(0xFF6F35A5),
+        style: TextStyle(
+          color: Colors.black
+        ),
         decoration: InputDecoration(
           icon: Icon(
             icon,
             color: Color(0xFF6F35A5),
           ),
           hintText: hintText,
+          hintStyle: TextStyle(
+            color: Colors.black
+          ),
           border: InputBorder.none,
         ),
         onChanged: onChange,
