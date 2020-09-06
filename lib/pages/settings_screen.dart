283c0489517d3b0827841cd3d159341b9282b8cd
Aman <aksinha.101010@gmail.com>
2020-09-06 17:26:59 +0530

    added ui for changing user details and password

--- lib/pages/settings_screen.dart
+++ lib/pages/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:SafeShopper/utils/auth_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -161,12 +162,28 @@ class SettingsPage extends StatelessWidget {
                             title: Text('Change Details'),
                             leading: Icon(Icons.update),
                             trailing: Icon(Icons.arrow_forward_ios),
+                            onTap: () {
+                              Scaffold.of(context).showBottomSheet(
+                                (context) => ChangeDetailsAndPassword(
+                                  child: ChangeUserDetails(),
+                                  height: 450,
+                                ),
+                              );
+                            },
                           ),
                           Divider(),
                           ListTile(
                             title: Text('Change Password'),
                             leading: Icon(Icons.lock),
                             trailing: Icon(Icons.arrow_forward_ios),
+                            onTap: () {
+                              Scaffold.of(context).showBottomSheet(
+                                (context) => ChangeDetailsAndPassword(
+                                  child: ChangePassword(),
+                                  height: 300,
+                                ),
+                              );
+                            },
                           ),
                           Divider(),
                           ListTile(
@@ -197,6 +214,193 @@ class SettingsPage extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class UserDetailsTile extends StatelessWidget {
+  const UserDetailsTile(
+      {Key key,
+      @required this.field,
+      @required this.value,
+      @required this.icon})
+      : super(key: key);
+
+  final String field;
+  final String value;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(field),
+      subtitle: Text(value),
+      leading: Icon(
+        icon,
+        size: 30,
+      ),
+    );
+  }
+}
+
+class ChangeDetailsAndPassword extends StatelessWidget {
+  @override
+  ChangeDetailsAndPassword(
+      {Key key, @required this.child, @required this.height});
+
+  final double height;
+  final Widget child;
+
+  Widget build(BuildContext context) {
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+      child: Container(
+        height: height,
+        child: Card(
+          child: child,
+          elevation: 10,
+          margin: EdgeInsets.all(20),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ChangeUserDetails extends StatefulWidget {
+  @override
+  _ChangeUserDetailsState createState() => _ChangeUserDetailsState();
+}
+
+class _ChangeUserDetailsState extends State<ChangeUserDetails> {
+  
+  String _name;
+  String _phone;
+  String _address;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          'Change Details',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+        TextInputField(
+          hintText: 'Name',
+          icon: Icons.person,
+          onChanged: (value) {
+            _name = value;
+          },
+        ),
+        TextInputField(
+          hintText: 'Phone',
+          icon: Icons.phone,
+          onChanged: (value) {
+            _phone = value;
+          },
+        ),
+        TextInputField(
+          hintText: 'Address',
+          icon: Icons.landscape,
+          onChanged: (value) {
+            _address = value;
+          },
+        ),
+        FlatButton(
+          onPressed: () {},
+          color: Theme.of(context).accentColor,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: Text(
+            'Change Details',
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class TextInputField extends StatelessWidget {
+  const TextInputField(
+      {Key key,
+      @required this.hintText,
+      @required this.icon,
+      @required this.onChanged})
+      : super(key: key);
+
+  final IconData icon;
+  final String hintText;
+  final void Function(String) onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.grey[400],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            icon: Icon(icon),
+            hintText: hintText,
+            hintStyle: TextStyle(
+              color: Colors.black,
+            )),
+        onChanged: onChanged,
+      ),
+    );
+  }
+}
+
+class ChangePassword extends StatefulWidget {
+  @override
+  _ChangePasswordState createState() => _ChangePasswordState();
+}
+
+class _ChangePasswordState extends State<ChangePassword> {
+  String _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          'Change Password',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+        TextInputField(
+          hintText: 'New Password',
+          icon: Icons.lock,
+          onChanged: (value) {
+            _password = value;
+          },
+        ),
+        FlatButton(
+          onPressed: () {},
+          color: Theme.of(context).accentColor,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: Text(
+            'Change Password',
+          ),
+        )
+      ],
     );
   }
 }
