e4e8fa15b2f4f84d3a0043bd7f64bc8848285708
Aman <aksinha.101010@gmail.com>
2020-09-07 23:03:36 +0530

    fade animate for given child widget

---
+++ lib/utils/animate_child.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class AnimateChild extends StatefulWidget {
+  final Widget child;
+
+  AnimateChild({@required this.child});
+
+  @override
+  _AnimateChildState createState() => _AnimateChildState();
+}
+
+class _AnimateChildState extends State<AnimateChild>
+    with SingleTickerProviderStateMixin {
+      
+  AnimationController _animationController;
+
+  Animation<double> _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    _animation = CurvedAnimation(
+      curve: Curves.easeIn,
+      parent: _animationController,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _animationController.reset();
+    _animationController.forward();
+    return FadeTransition(
+      opacity: _animation,
+      child: widget.child,
+    );
+  }
+}
